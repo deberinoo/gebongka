3f5fafd142db50fb98ea73afb8199da604d1691e
Deborah Lu <deberinoo@users.noreply.github.com>
2023-01-01 22:39:58 +0800

    added .gitignore, set up flask server + jinja templates

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# ----- routes -----
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about-us')
+def about_us():
+    return render_template('about-us.html')
+
+@app.route('/apps')
+def apps():
+    return render_template('apps.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

d79a1608ba42160deabfe7324c7aa6b69d7b0de5
Deborah Lu <deberinoo@users.noreply.github.com>
2023-01-05 23:06:52 +0800

    reformatted files, added flash messaging

---
+++ website/auth.py
@@ -0,0 +1,47 @@
+from flask import Flask, Blueprint, render_template, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
+
+from .forms import LoginForm, RegisterForm
+from .models import User
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                flash('Logged in successfully!', category='success')
+                login_user(user)
+                return redirect(url_for('views.profile'))
+        else:
+            flash('Email does not exist.', category='error')
+    
+    return render_template("auth/login.html", user=current_user, form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        flash('Account created!', category='success')
+        return redirect(url_for('views.profile'))
+    
+    return render_template('auth/register.html', form=form)

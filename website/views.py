8350fc299506ffa32a917f0e83758054ac23a91c
Deborah Lu <deberinoo@users.noreply.github.com>
2023-01-21 21:23:42 +0800

    edited front-end, removed unnecessary pages

--- website/views.py
+++ website/views.py
@@ -13,21 +13,10 @@ def handle_404(err):
 def index():
     return render_template("index.html")
 
-@views.route('/apps')
-def apps():
-    return render_template("apps.html")
-
-@views.route('/about-us')
-def about_us():
-    return render_template("about-us.html")
 
 @views.route('/features')
 def features():
     return render_template("features.html")
-
-@views.route('/pages')
-def pages():
-    return render_template("pages.html")    
 
 @views.route('/profile')
 @login_required

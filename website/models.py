bd4decf4ef95c726e28dc82817e9f1fe8bcae47b
Deborah Lu <deberinoo@users.noreply.github.com>
2023-01-27 11:22:11 +0800

    fixed typo

--- website/models.py
+++ website/models.py
@@ -2,7 +2,7 @@ from . import db
 from flask_login import UserMixin
 
 
-class User(db.Model, UserMixin):
+class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
